c0a1e74d7289992f5c1938bca95b456dfd3afc9a
Harold Gomez <xagobo14@gmail.com>
2021-09-20 14:57:18 -0500

    ensamble: creacion del backend para el front HG

--- Fiveware/src/main/java/co/edu/unbosque/Servlet.java
+++ Fiveware/src/main/java/co/edu/unbosque/Servlet.java
@@ -1,51 +1,62 @@
 package co.edu.unbosque;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Servlet
- */
-@WebServlet("/Servlet")
+@WebServlet("/FiveServlet")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Servlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
+    
+    public void validarUsuarios(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			ArrayList<Usuarios> lista = UsuariosJSON.getJSON();
+			request.setAttribute("lista", lista);
+			String usua = request.getParameter("txtusuario");
+			String pass = request.getParameter("txtpassword");
+			int respuesta = 0;
+			for (Usuarios usuario : lista) {
+				if (usuario.getUsuario().equals(usua) && usuario.getPassword().equals(pass)) {
+					request.setAttribute("usuario", usuario);
+					request.getRequestDispatcher("/inicio.jsp").forward(request, response);
+					respuesta = 1;
+				}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String usuario = request.getParameter("txtUser");
-        String password = request.getParameter("txtPasswd");
-        if (usuario != null && password != null) {
-            response.getWriter().append("Bienvenido " + usuario + " a la Tienda Fourware");
-        } else {
-            response.getWriter().append("Complete los campos por favor");
-        }
+			}
+
+			if (respuesta == 0) {
+				request.getRequestDispatcher("/index.jsp").forward(request, response);
+				System.out.println("No se encontraron datos");
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String accion = request.getParameter("accion");
+		if (accion.equals("Ingresar")) {
+			this.validarUsuarios(request, response);
+		}
+	}
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String usuario = request.getParameter("txtUser");
-        String password = request.getParameter("txtPasswd");
-        if (usuario != null && password != null) {
-            response.getWriter().append("Bienvenido " + usuario + " a la Tienda Fourware");
-        } else {
-            response.getWriter().append("Complete los campos por favor");
-        }
+		String accion = request.getParameter("accion");
+		if (accion.equals("Ingresar")) {
+			this.validarUsuarios(request, response);
+		}
 	}
 
 }
